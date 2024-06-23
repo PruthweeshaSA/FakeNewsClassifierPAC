f413a6a345c6673c4ed46d0d0ed72481b764b99e
PruthweeshaSA <pruthweeshasa@gmail.com>
2024-06-24 00:05:59 +0530

    first commit

---
+++ .ipynb_checkpoints/classifier-checkpoint.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import itertools
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+data=pd.read_csv('news.csv')
+data.head()
+
+#lb=df.label
+lb=data.label
+lb.head()
+
+x_train,x_test,y_train,y_test=train_test_split(data['text'], lb, test_size=0.2, random_state=7)
+
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
+tfidf_test=tfidf_vectorizer.transform(x_test)
+ 
+pac=PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+ 
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+
+for i in y_pred:
+  print(i)
+ 
+print("Accuracy: ",round(score*100,2),"%")
+
+print(confusion_matrix(y_test,y_pred, labels=['FAKE','REAL']))
